3207bf305bedaebfc42bf6572340aa536507a641
halfmoon <develop@hmfactory.net>
2016-10-30 20:17:11 +0900

    関数

---
+++ func1.go
@@ -0,0 +1,24 @@
+// 関数
+
+package main
+
+import (
+	"fmt"
+)
+
+//func hi(name string) string {
+//	//fmt.Println("hi!" + name)
+//	msg := "hi!" + name
+//	return msg
+//}
+
+func hi(name string) (msg string) {
+	//fmt.Println("hi!" + name)
+	msg = "hi!" + name
+	return
+}
+
+func main() {
+	//hi("hmFactory")
+	fmt.Println(hi("hmfactory"))
+}
